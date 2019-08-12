95411f86a16c49b00a01d0c344e8dc82701918fd
chinhle <chinhle@chinhs-mbp.attlocal.net>
2019-08-12 13:08:51 -0400

    Remove @ symbol

--- app/models/spreedly.rb
+++ app/models/spreedly.rb
@@ -3,6 +3,6 @@ require 'json'
 require 'uri'
 require 'net/http'
 
-@ENV_KEY      = Rails.configuration.spreedly['environment_key']
+ENV_KEY      = Rails.configuration.spreedly['environment_key']
 ACCESS_SECRET = Rails.configuration.spreedly['access_secret']
 GATEWAY_TOKEN = Rails.configuration.spreedly['gateway_token']
